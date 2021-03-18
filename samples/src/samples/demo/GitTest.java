480c7c3dd3e307df7e56fb3493b7390c86abc13d
Ahmad Nabil <billonabillo@gmail.com>
2021-03-18 16:47:58 +0800

    Initial Commit

---
+++ samples/src/samples/demo/GitTest.java
@@ -0,0 +1,14 @@
+package samples.demo;
+
+public class GitTest {
+
+	public GitTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Git is fun");
+
+	}
+
+}
