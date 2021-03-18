f3572e3a9b509b0b6e709a938d67eb1b3475f034
Ahmad Nabil <billonabillo@gmail.com>
2021-03-18 16:57:03 +0800

    location commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+public class Location {
+private int length;
+	public Location(int length) {
+		this.setLength(length);
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+}
